385c194d8d274cde381f7ca9f801c4d74784dc02
sholtkamp <S.Kokon93@gmail.com>
2019-06-26 11:26:57 +0200

    Update preprocessing.py
    added: Reprojection to UTM 32N

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ import os
 import csv
 import numpy as np
 from osgeo import ogr
+import processing
 
 #csvPath = "./eagle_owl_csv/eagle_owl.csv"
 csvPath = "/Users/user/Desktop/eagle_owl/eagle_owl.csv"
@@ -13,6 +14,15 @@ with open(csvPath) as csvfile:
 
 reduced_data = data[:,[1,3,8,10,]]  #Drop all columns except for declared indices
 reduced_data = np.delete(reduced_data,[4,18] ,axis=0, )  # Drop rows with empty fields
+
+# Reproject data to UTM 32N
+# Define reprojection parameters
+parameter = {'INPUT': '/Users/user/Desktop/eagle_owl/eagle_owl_shp/lines.shp', 'TARGET_CRS': 'EPSG:4647',
+                 'OUTPUT': '/Users/user/Desktop/eagle_owl/eagle_owl_shp/lines_32N.shp'}
+
+# Run reprojection
+processing.run('qgis:reprojectlayer', parameter)
+
 
 ##Parsing SHP file and accessing attributes
 #shpFile = "./eagle_owl_shp/lines.shp"
