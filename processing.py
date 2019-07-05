1978056e8feecb64f85820872533d9bfb5f755de
sholtkamp <S.Kokon93@gmail.com>
2019-07-05 14:43:09 +0200

    minor adjustments

--- processing.py
+++ processing.py
@@ -69,6 +69,8 @@ class data_processing():
                 feat_length_year = feat_length * percentage
                 total_length_m += feat_length_year
                 count_m += 1
+
+                #adding field with yearly_distance
         
         if count_m > 0:
             avg_distance_m = total_length_m/count_m
@@ -140,6 +142,18 @@ class data_processing():
         #delta_height = abs(round(avg_height_f - avg_height_m, 3))
         #print("Difference between sex-based averages is: " + str(delta_height) + " m") 
 
+def make_predictions(input_layer):
+    for feature in input_layer:
+
+    feature.getField
+    x = lade ids in numpy array
+    y = lade attribut 1 in np array
+
+    selektiere 10 aus x, y
+
+    plt.scatter()
+
 pro = data_processing()
 pro.processing_setup()
 pro.calc_distance_differences()
+pro.make_predictions() # braucht den gesamten lines layer als input
