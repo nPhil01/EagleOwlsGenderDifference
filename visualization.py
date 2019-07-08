de230547da5b2a4d36804f3910547b9274cadd17
SarahLechler <lechler.sarah@gmail.com>
2019-07-08 19:50:23 +0200

    add boxplots for height and distance

--- visualization.py
+++ visualization.py
@@ -12,19 +12,37 @@ layer = QgsVectorLayer(shpFile, "working_layer", "ogr")
 
 femaleSpeed = []
 maleSpeed = []
+femaleHeight = []
+maleHeight = []
+femaleDis = []
+maleDis = []
+
 for feat in layer.getFeatures():
     if(feat["sex"]=="f"):
         femaleSpeed.append(float(feat["avg_speed"]))
+        femaleHeight.append(float(feat["avg_height"]))
+        femaleDis.append(float(feat["yearly_dis"]))
     if(feat["sex"]=="m"):
         maleSpeed.append(float(feat["avg_speed"]))
-
-print(femaleSpeed)
-print(maleSpeed)
+        maleHeight.append(float(feat["avg_height"]))
+        maleDis.append(float(feat["yearly_dis"]))
 
 data = [maleSpeed, femaleSpeed]
-fig7, ax7 = plt.subplots()
-ax7.set_title('Average Speed of Male and Female')
-ax7.boxplot(data)
+fig1, ax1 = plt.subplots()
+ax1.set_title('Average Speed of Male and Female')
+ax1.boxplot(data)
+plt.xticks([1,2], ["Male", "Female"])
+
+data = [maleDis, femaleDis]
+fig2, ax2 = plt.subplots()
+ax2.set_title('Average Distance travelled per year of Male and Female')
+ax2.boxplot(data)
+plt.xticks([1,2], ["Male", "Female"])
+
+data = [maleHeight, femaleHeight]
+fig3, ax3 = plt.subplots()
+ax3.set_title('Average Height of Male and Female')
+ax3.boxplot(data)
 plt.xticks([1,2], ["Male", "Female"])
 
 plt.show()
